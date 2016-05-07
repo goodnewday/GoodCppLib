961d2049aebc32cec1c1f927f51a3f19b51efafb
goodnewday <1028199089@qq.com>
2016-05-07 21:09:54 +0800

    Signed-off-by: goodnewday

---
+++ Network/TCPClient.cpp
@@ -0,0 +1,35 @@
+#include<netinet/in.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<strings.h>
+#include<stdio.h>
+
+void TCPClient()
+{
+	int             listenfd;
+	const int        on = 1;
+	struct sockaddr_in hints;
+	char buf[100] = {"0sdfsdfsd"};
+	int ret = 0;
+
+	bzero(&hints, sizeof(struct sockaddr_in));
+	hints.sin_addr.s_addr = INADDR_ANY;
+	hints.sin_family = AF_INET;
+	hints.sin_port = htons(8899);
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+	if (connect(listenfd, (struct sockaddr*)&hints, sizeof( struct sockaddr_in)) != 0)
+		printf(" connect fail\n");
+
+	ret = send(listenfd, buf, 100,0);
+
+}
+
+int main()
+{
+	TCPClient();
+
+	return 0;
+}
+
---
+++ Network/TCPServer.cpp
@@ -0,0 +1,41 @@
+#include<netinet/in.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<strings.h>
+#include<stdio.h>
+
+
+int TCPServer()
+{
+	int   listenfd, connfd;
+	struct sockaddr_in  cliaddr;
+
+	socklen_t    clilen = sizeof( struct sockaddr_in);
+	struct sockaddr_in hints;
+	char buf[500] = {0};
+
+	bzero(&hints, sizeof(struct sockaddr_in));
+	hints.sin_addr.s_addr =  INADDR_ANY;
+	hints.sin_family = AF_INET;
+	hints.sin_port = htons(8899);
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (bind(listenfd, (struct sockaddr*)&hints, sizeof( struct sockaddr_in)) != 0) {
+		printf(" bind fail\n");
+	}
+	listen(listenfd, 10);
+	connfd = accept(listenfd, (struct sockaddr*)&cliaddr, &clilen);
+
+	int ret = 0;
+	ret = recv(connfd, buf, 500, 0);
+	printf("buf = %s\n", buf);
+
+	return 0;
+}
+
+int main()
+{
+	TCPServer();
+
+	return 0;
+}
