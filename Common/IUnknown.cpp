7ebd25539c371fa1a117c0ef6daf3cc4ab6bc696
goodnewday <1028199089@qq.com>
2016-05-10 23:29:00 +0800

    Signed-off-by: goodnewday

---
+++ Common/IUnknown.cpp
@@ -0,0 +1,46 @@
+#include"IUnKnown.h"
+
+IUnKnown::IUnKnown()
+{
+	refcount = 0;
+}
+IUnKnown::~IUnKnown()
+{
+	
+}
+
+int32_t IUnKnown::AddRef()
+{
+	refcount++;
+
+	return refcount;
+}
+
+int32_t IUnKnown::Release()
+{
+	if (--refcount == 0)
+	{	
+		delete this;		
+	}
+
+	return refcount;
+}
+
+//规则:
+//1.返回接口指针时调用AddRef
+//2.接口用完后调用Release
+//3.赋值后调用AddRef.
+
+
+
+//g++ *cpp *.h -std=c++11
+
+int main()
+{
+	IUnKnown *IUN = new IUnKnown;
+	
+	IUN->AddRef();
+	
+	IUN->Release();
+	return 0;
+}
---
+++ Common/IUnknown.h
@@ -0,0 +1,14 @@
+
+#include <atomic>
+
+class IUnKnown
+{
+	public:
+		IUnKnown();
+		~IUnKnown();
+		int32_t  AddRef();
+		int32_t  Release();
+		
+	private:
+		std::atomic<int32_t>  refcount;
+};
